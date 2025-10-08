5dc75e3a2c1d2388bdc5298c034e31bc74a455de
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-10-08 12:18:16 +0900

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main (String[] args){
+  int[] scores;
+  scores + new int [3];
+
+  scores[0] = 1;
+  scores[1] = 5;
+  scores[2] = 10;
+
+System.out.println(scores[0]);
+System.out.println(scores[1]);
+System.out.println(scores[2]);
+  }
+}
