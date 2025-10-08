ed17a012f717a8814710f2bb3c716ef76d5ac68e
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-10-08 12:27:11 +0900

    Refactor scores to use ArrayList

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
+
 class Main {
   public static void main (String[] args){
-  int[] scores;
-  scores + new int [3];
+   ArrayList<Integer> scores = new ArrayList<Integer>();
 
-  scores[0] = 1;
-  scores[1] = 5;
-  scores[2] = 10;
+    scores.add(1);
+    scores.add(5);
+    scores.add(10);
+    scores.add(15);
 
-System.out.println(scores[0]);
-System.out.println(scores[1]);
-System.out.println(scores[2]);
+    System.out.println(scores.get(0));
+    System.out.println(scores.get(1));
+    System.out.println(scores.get(2));
+    System.out.println(scores.get(3));
+
+    
   }
 }
